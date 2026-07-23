c52c74743c9c1bcf028025362fd8e660f996ab8d
Yongquan Luo <mrluoyq@163.com>
2026-07-23 18:34:40 +0800

    Refactor config file handling and improve error checks
    Refactor to use a variable for the config file name and sort filenames for better consistency. Added error handling for concatenation of measurement sets.

--- TM2/pre_process.py
+++ TM2/pre_process.py
@@ -2,11 +2,18 @@ import glob
 import os
 import ast
 
+config_file = "tclean_params.txt"
+print("The config file is:", config_file)
 
-print("The config file is:", 'tclean_params.txt')
 params = {}
-with open('tclean_params.txt', 'r') as f:
+with open(config_file, 'r') as f:
     for line in f:
+    
+	line = line.strip()
+	
+	if not line or line.startswith("#"):
+            continue
+  
         if '=' in line:
             key, val = line.split('=', 1)
             key = key.strip()
@@ -21,9 +28,10 @@ field = params['field']
 print("The array configuration is:", array)
 print("The splitting field is:", field)
 
+
+
 spw = ""
 spwname = ""
-
 
 #### for observation of band6, may be changed for other band
 if array == "ACA":
@@ -36,7 +44,8 @@ if array == "TM1" or array == "TM2":
 ####
 
 
-filenames = glob.glob("../*.ms.split.cal")
+filenames = sorted(glob.glob("../*.ms.split.cal"))
+
 print("Start splitting", filenames)
 ##########   split the field
 for i in range(len(filenames)):
@@ -49,7 +58,9 @@ for i in range(len(filenames)):
     )
 
 ########  transform the frame to LSRK
-filenames_split = glob.glob("./*" + "." + field + "." + array + "." + spwname)
+filenames_split = sorted(
+    glob.glob("./*." + field + "." + array + "." + spwname)
+)
 
 for i in range(len(filenames_split)):
     listobs(vis = filenames_split[i], listfile = filenames_split[i] + ".listobs.txt", overwrite = False)
@@ -63,18 +74,55 @@ for i in range(len(filenames_split)):
         datacolumn = 'data'
     )
 
-filenames_LSRK = glob.glob("./*" + "." + field + "." + array + "." + spwname + ".LSRK")
+filenames_LSRK = sorted(
+    glob.glob("./*." + field + "." + array + "." + spwname + ".LSRK")
+)
+
 for i in range(len(filenames_LSRK)):
     listobs(vis = filenames_LSRK[i], listfile = filenames_LSRK[i] + ".listobs.txt", overwrite = False)
 
 
 ########  combine data
+import shutil
 
 concatvis = field + '.ms' + '.' + array + '.' + spwname
+print("LSRK measurement sets:", filenames_LSRK)
 
-concat(vis = filenames_LSRK,
-       concatvis = field + '.ms' + '.' + array + '.' + spwname,
-       freqtol = '1MHz'
-       )
+if len(filenames_LSRK) == 0:
+    raise RuntimeError(
+        "No LSRK measurement sets were found. "
+        "Please check the field, array, and SPW settings."
+    )
+elif len(filenames_LSRK) == 1:
+    print("Only one measurement set was found; concat will be skipped.")
 
-listobs(vis = concatvis, listfile = concatvis + ".listobs.txt", overwrite = False)
+    inputvis = filenames_LSRK[0]
+
+    #
+    if os.path.abspath(inputvis) != os.path.abspath(concatvis):
+
+        if os.path.exists(concatvis):
+            raise RuntimeError(
+                "Output measurement set already exists: {}".format(concatvis)
+            )
+
+        shutil.move(inputvis, concatvis)
+else:
+    print("Combining {} measurement sets.".format(len(filenames_LSRK)))
+
+    if os.path.exists(concatvis):
+        raise RuntimeError(
+            "Output measurement set already exists: {}".format(concatvis)
+        )
+
+    concat(
+        vis=filenames_LSRK,
+        concatvis=concatvis,
+        freqtol="1MHz"
+    )
+
+listobs(
+    vis = concatvis,
+    listfile = concatvis + ".listobs.txt",
+    overwrite = True
+)
